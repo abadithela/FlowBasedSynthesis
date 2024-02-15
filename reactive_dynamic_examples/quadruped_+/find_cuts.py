1eb22272488b06de0b07688f04ba7bca79d6afa8
Josefine G <51681591+jgraeb@users.noreply.github.com>
2024-02-15 13:41:08 -0800

    used mixed for quadruped + example

--- reactive_dynamic_examples/quadruped_+/find_cuts.py
+++ reactive_dynamic_examples/quadruped_+/find_cuts.py
@@ -18,7 +18,7 @@ MILP for reactive obstacles for the following grid:
 import sys
 sys.path.append('../..')
 
-from reactive_dynamic_examples.utils.solve_problem import solve_problem
+from reactive_dynamic_examples.utils.solve_problem import solve_problem_mixed
 from reactive_dynamic_examples.utils.get_graphs import get_graphs
 from problem_data import *
 
@@ -26,9 +26,11 @@ def find_cuts():
     intstr = ''.join('%s = %s, ' % (val,key) for (key,val) in INTS.items())
     print('S = '+str(INIT)+', '+intstr+' T = '+str(GOALS))
 
+    static_area = [(0,0), (0,1), (1,0), (3,0), (4,0), (4,1), (0,3), (0,4), (1,4), (3,4), (4,4), (4,3)]
+
     virtual, system, b_pi, virtual_sys = get_graphs(SYS_FORMULA, TEST_FORMULA, MAZEFILE, INIT, INTS, GOALS)
 
-    exit_status, annot_cuts, flow, bypass, GD = solve_problem(virtual, system, b_pi, virtual_sys)
+    exit_status, annot_cuts, flow, bypass, GD = solve_problem_mixed(virtual, system, b_pi, virtual_sys, static_area=static_area)
     print('exit status {0}'.format(exit_status))
 
     return annot_cuts, GD
--- reactive_dynamic_examples/utils/solve_problem.py
+++ reactive_dynamic_examples/utils/solve_problem.py
@@ -7,6 +7,8 @@ import time
 
 from optimization.find_bypass_flow import find_fby
 from optimization.milp_reactive_gurobipy import solve_max_gurobi
+from optimization.milp_mixed_gurobipy import solve_max_gurobi as solve_max_gurobi_mixed
+
 
 from components.setup_graphs import setup_nodes_and_edges
 from components.plotting import highlight_cuts, plot_flow_on_maze, make_history_plots
@@ -16,6 +18,40 @@ def solve_problem(virtual, system, b_pi, virtual_sys, print_solution=True, plot_
 
     ti = time.time()
     exit_status, ftest, d, flow = solve_max_gurobi(GD, SD)
+    tf = time.time()
+    del_t = tf-ti
+
+    if exit_status == 'opt':
+        fby = find_fby(GD, d)
+        bypass_flow = sum([fby[j] for j in fby.keys() if j[1] in GD.sink])
+        cuts = [x for x in d.keys() if d[x] >= 0.9]
+
+        if print_solution:
+            print('Cut {} edges in the virtual game graph.'.format(len(cuts)))
+            print('The max flow through I is {}'.format(flow))
+            print('The bypass flow is {}'.format(bypass_flow))
+            for cut in cuts:
+                print('Cutting {0} to {1}'.format(GD.node_dict[cut[0]], GD.node_dict[cut[1]]))
+
+        if plot_results:
+            # highlight_cuts(cuts, GD, SD, virtual, virtual_sys)
+            sys_cuts = [(GD.node_dict[cut[0]][0], GD.node_dict[cut[1]][0]) for cut in cuts]
+            # plot_flow_on_maze(system.maze, sys_cuts)
+            make_history_plots(cuts, GD, system.maze)
+
+        # from ipdb import set_trace as st
+        # st()
+        annot_cuts = [(GD.node_dict[cut[0]], GD.node_dict[cut[1]]) for cut in cuts]
+        # st()
+        return exit_status, annot_cuts, flow, bypass_flow, GD
+    else:
+        return exit_status, [], [], None, GD
+
+def solve_problem_mixed(virtual, system, b_pi, virtual_sys, static_area, print_solution=True, plot_results=True):
+    GD, SD = setup_nodes_and_edges(virtual, virtual_sys, b_pi)
+
+    ti = time.time()
+    exit_status, ftest, d, flow = solve_max_gurobi_mixed(GD, SD, static_area = static_area)
     tf = time.time()
     del_t = tf-ti
 
