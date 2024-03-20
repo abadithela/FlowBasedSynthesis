81b1f157ee6cf4abad0532c15c0502afe3c8891a
Josefine G <51681591+jgraeb@users.noreply.github.com>
2024-03-20 13:12:30 -0700

    add controller

--- static_examples/utils/agents_w_fuel.py
+++ static_examples/utils/agents_w_fuel.py
@@ -24,74 +24,81 @@ class Quadruped:
 
     def find_controller(self, maze):
         self.maze = maze
-        logging.basicConfig(level=logging.WARNING)
-        logging.getLogger('tulip.spec.lexyacc').setLevel(logging.WARNING)
-        logging.getLogger('tulip.synth').setLevel(logging.WARNING)
-        logging.getLogger('tulip.interfaces.omega').setLevel(logging.WARNING)
+        try:
+            # load the controller
+            from robot_controller import AgentCtrl
+            self.controller = AgentCtrl()
+            print('successfully loaded system controller')
+            return self.controller
+        except:
+            logging.basicConfig(level=logging.WARNING)
+            logging.getLogger('tulip.spec.lexyacc').setLevel(logging.WARNING)
+            logging.getLogger('tulip.synth').setLevel(logging.WARNING)
+            logging.getLogger('tulip.interfaces.omega').setLevel(logging.WARNING)
 
-        sys_vars = {}
-        sys_vars['x'] = (0,maze.len_x)
-        sys_vars['z'] = (0,maze.len_z)
-        sys_vars['f'] = (0,MAX_FUEL)
-        sys_init = {'x = '+str(self.x)+' && z = '+str(self.z)+' && f = '+str(self.f)}
-        sys_prog = set()
-        goalstr = ''
-        reduced_GOALS = list(set([goal[0] for goal in self.goals]))
-        for goal in reduced_GOALS:
-            goalstr += '(z = '+str(goal[0])+' && x = '+str(goal[1])+') || '
-        goalstr = goalstr[:-4]
-        sys_prog |= {goalstr}
-        sys_safe = set()
+            sys_vars = {}
+            sys_vars['x'] = (0,maze.len_x)
+            sys_vars['z'] = (0,maze.len_z)
+            sys_vars['f'] = (0,MAX_FUEL)
+            sys_init = {'x = '+str(self.x)+' && z = '+str(self.z)+' && f = '+str(self.f)}
+            sys_prog = set()
+            goalstr = ''
+            reduced_GOALS = list(set([goal[0] for goal in self.goals]))
+            for goal in reduced_GOALS:
+                goalstr += '(z = '+str(goal[0])+' && x = '+str(goal[1])+') || '
+            goalstr = goalstr[:-4]
+            sys_prog |= {goalstr}
+            sys_safe = set()
 
-        # add the dynamics for the system
-        dynamics_spec =  maze.dynamics_specs_w_fuel('x','z','f')
-        sys_safe |= dynamics_spec
+            # add the dynamics for the system
+            dynamics_spec =  maze.dynamics_specs_w_fuel('x','z','f')
+            sys_safe |= dynamics_spec
 
-        # Safety spec, never run out of fuel
-        sys_safe |= {'f > 0'}
+            # Safety spec, never run out of fuel
+            sys_safe |= {'f > 0'}
 
-        # add auxiliary formula (re-written reaction spec)
-        if self.aux_formula['var']:
-            for var in self.aux_formula['var']:
-                sys_vars[var] = 'boolean'
-        if self.aux_formula['safe']:
-            sys_safe |= self.aux_formula['safe']
-        if self.aux_formula['init']:
-            sys_init |= self.aux_formula['init']
-        if self.aux_formula['prog']:
-            sys_prog |= self.aux_formula['prog']
+            # add auxiliary formula (re-written reaction spec)
+            if self.aux_formula['var']:
+                for var in self.aux_formula['var']:
+                    sys_vars[var] = 'boolean'
+            if self.aux_formula['safe']:
+                sys_safe |= self.aux_formula['safe']
+            if self.aux_formula['init']:
+                sys_init |= self.aux_formula['init']
+            if self.aux_formula['prog']:
+                sys_prog |= self.aux_formula['prog']
 
-        env_vars = {}
-        env_safe = set()
-        env_init = set()
-        env_prog = set()
+            env_vars = {}
+            env_safe = set()
+            env_init = set()
+            env_prog = set()
 
-        spc = spec.GRSpec(env_vars, sys_vars, env_init, sys_init,
-                        env_safe, sys_safe, env_prog, sys_prog)
+            spc = spec.GRSpec(env_vars, sys_vars, env_init, sys_init,
+                            env_safe, sys_safe, env_prog, sys_prog)
 
-        print(spc.pretty())
+            print(spc.pretty())
 
-        spc.moore = False
-        spc.qinit = r'\A \E'
-        if not synth.is_realizable(spc, solver='omega'):
-            print("Not realizable.")
+            spc.moore = False
+            spc.qinit = r'\A \E'
+            if not synth.is_realizable(spc, solver='omega'):
+                print("Not realizable.")
 
-        else:
-            ctrl = synth.synthesize(spc, solver='omega')
-        # dunp the controller
-        controller_namestr = "robot_controller"+str(self.index)+".py"
-        dumpsmach.write_python_case(controller_namestr, ctrl, classname="AgentCtrl")
-        # # load the controller
-        # from controller_namestr import AgentCtrl
-        # M = AgentCtrl()
-        self.index += 1
+            else:
+                ctrl = synth.synthesize(spc, solver='omega')
+            # dunp the controller
+            controller_namestr = "robot_controller"+str(self.index)+".py"
+            dumpsmach.write_python_case(controller_namestr, ctrl, classname="AgentCtrl")
+            # # load the controller
+            # from controller_namestr import AgentCtrl
+            # M = AgentCtrl()
+            self.index += 1
 
-        # print(dumpsmach.python_case(g, classname='AgentCtrl', start=))
-        exe_globals = dict()
-        exec(dumpsmach.python_case(ctrl, classname='AgentCtrl'), exe_globals)
-        M = exe_globals['AgentCtrl']()  # previous line creates the class `AgentCtrl`
-        self.controller = M
-        return M
+            # print(dumpsmach.python_case(g, classname='AgentCtrl', start=))
+            exe_globals = dict()
+            exec(dumpsmach.python_case(ctrl, classname='AgentCtrl'), exe_globals)
+            M = exe_globals['AgentCtrl']()  # previous line creates the class `AgentCtrl`
+            self.controller = M
+            return M
 
     def agent_move(self):
         output = self.controller.move()
